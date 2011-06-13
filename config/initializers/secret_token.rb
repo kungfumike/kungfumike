98854e90f5db03a71316af8f137d98b8ea41e0a8
Mike Thompson <michael.p.thompson@gmail.com>
2011-06-13 11:09:27 -0700

    Adding in the config directory, just with the db file ignored

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kungfumike::Application.config.secret_token = 'd6e41dd132c719a567eebe7bc4394cca247fc64c213747775f51def28df72b8d76d5cb19f957677172b024827647d612a08b8444c2628c1038342ad418c84c2b'
